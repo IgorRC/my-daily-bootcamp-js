c6d381d8f4258481486abe5f0a8e3b6842278d98
Marklnz1 <jhon4lnz@gmail.com>
2022-07-12 16:13:52 -0500

    transition modal

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,36 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-      
+let modal_content = document.querySelector(".modal-content");
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var learning_input = document.querySelector(".learning-input");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close-icon")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+learning_input.onclick = function() {
+  modal.style.visibility = "visible";
+  
+  console.log(modal_content)
+  modal_content.classList.add("move-model")
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.visibility = "hidden";
+    modal_content.classList.remove("move-model")
+
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.visibility = "hidden";
+    modal_content.classList.remove("move-model")
+
   }
 }
+
 
 function maximo(campo,limite){
     if(campo.value.length>=limite){
