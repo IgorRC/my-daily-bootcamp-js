9e5f3c15a2e0161f3d57e223caa2001332e05e7e
m4rtintrujillo <m4rtin.trujillo@gmail.com>
2022-07-13 18:01:57 -0500

    creacion de popup y modal delete

---
+++ js/delete-modal.js
@@ -0,0 +1,46 @@
+let popUpBox = document.getElementById("popUpBox");
+ let estabotonoculto = true;
+let public_button = document.querySelector(".public-button");
+let modal_button_delete = document.getElementById("modal-button-delete");
+ let closemodal = document.getElementById("closemodal")
+
+
+public_button.addEventListener("click",togglebuttondots)
+
+
+function togglebuttondots()
+{
+  if (estabotonoculto )
+  {
+    
+    popUpBox.style.display = "block";
+    estabotonoculto =false;
+
+  }
+  else
+  {
+    popUpBox.style.display = "none";
+    estabotonoculto =true;
+  }
+}
+
+popUpBox.addEventListener("click",modaldelete)
+
+function modaldelete()
+{
+
+  modal_button_delete.style.display="flex";
+ popUpBox.style.display = "none";
+
+}
+
+closemodal.addEventListener("click",cerrar_modal)
+function cerrar_modal()
+{
+  
+  modal_button_delete.style.display="none";
+  
+
+
+}
+
